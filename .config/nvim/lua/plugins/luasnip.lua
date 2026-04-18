76cb0bdd92ed3f33871b95366e057014e2d9b527
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-04-18 02:56:16 +0200

    added lua snip

---
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,34 @@
+return {
+	"L3MON4D3/LuaSnip",
+	version = "v2.*",
+	build = "make install_jsregexp",
+	dependencies = {
+    "rafamadriz/friendly-snippets", -- actual snippet collection
+	},
+	config = function()
+		local ls = require("luasnip")
+		vim.keymap.set({"i", "s"}, "<C-K>", function()
+		  if ls.expand_or_jumpable() then
+			ls.expand_or_jump()
+		  end
+		end, { silent = true })
+
+		vim.keymap.set({"i", "s"}, "<C-J>", function()
+		  if ls.jumpable(-1) then
+			ls.jump(-1)
+		  end
+		end, { silent = true })
+
+		vim.keymap.set({"i", "s"}, "<C-L>", function()
+		  if ls.jumpable(1) then
+			ls.jump(1)
+		  end
+		end, { silent = true })
+
+		vim.keymap.set({"i", "s"}, "<C-E>", function()
+		  if ls.choice_active() then
+			ls.change_choice(1)
+		  end
+		end, { silent = true })
+	end
+}

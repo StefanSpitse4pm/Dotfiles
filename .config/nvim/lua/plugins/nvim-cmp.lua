e9932eb88a2cef29d167ae6e6b5a584e7700f99b
StefanSpitse4pm <stefan.spitse@student.nhlstenden.com>
2026-04-18 02:57:01 +0200

    added nvim-cmp using lua snip and intelij way of working

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,95 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    local has_words_before = function()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      if col == 0 then return false end
+      local text = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+      return text:sub(col, col):match("%s") == nil
+    end
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
+
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+
+          elseif has_words_before() then
+            cmp.complete()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            end
+
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+
+      sources = {
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+    })
+
+    -- cmdline completion
+    cmp.setup.cmdline(":", {
+      mapping = {
+        ["<Tab>"] = function()
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
+          else
+            cmp.complete()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            end
+          end
+        end,
+      },
+      sources = {
+        { name = "path" },
+        { name = "cmdline" },
+      },
+    })
+  end,
+}
